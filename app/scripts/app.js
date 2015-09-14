f10f9c4eb6d3f452d142bbe36ee9de33fdb8c083
blzsnake <kurskatm@gmail.com>
2015-09-14 08:22:37 +0300

    add pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,34 @@
 import 'svg4everybody';
 import $ from 'jquery';
+import 'slick-carousel';
 
 $(() => {
+	$('#mainSlider').slick({
+		prevArrow: '.arrow_main_prev'
+		, nextArrow: '.arrow_main_next'
+		, dots: true
+		, dotsClass: 'pagination'
+	});
+
+	$('#salesSlider').slick({
+		slidesToShow: 5
+		, prevArrow: '.arrow_sales_prev'
+		, nextArrow: '.arrow_sales_next'
+		, infinity: true
+	});
+
+	$('#newsSlider').slick({
+		slidesToShow: 5
+		, prevArrow: '.arrow_news_prev'
+		, nextArrow: '.arrow_news_next'
+		, infinity: true
+	});
+
+	$('#popularSlider').slick({
+		slidesToShow: 5
+		, prevArrow: '.arrow_popular_prev'
+		, nextArrow: '.arrow_popular_next'
+		, infinity: true
+	});
 	// Code here
 });
